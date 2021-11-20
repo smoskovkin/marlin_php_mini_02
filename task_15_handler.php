1028944d596714b49dc59d94744b07bac056dc81
xrr <xrr@email.com>
2021-11-20 16:58:20 +0300

    17 commit

--- task_15_handler.php
+++ task_15_handler.php
@@ -15,20 +15,6 @@ function db_query()
 }
 
 
-function is_link_exist_in_db($new_link)
-{
-    $link = db_query();
-
-    $sql_query = "SELECT * FROM images WHERE link = '$new_link'";
-
-    $result = mysqli_query($link, $sql_query) or die(mysqli_error($link));
-
-    $row = mysqli_fetch_assoc($result);
-
-    return isset($row);
-}
-
-
 function add_image_link_to_db($new_link)
 {
     $link = db_query();
@@ -55,21 +41,19 @@ if (isset($_FILES['img_input'])) {
 
 
 $upload_dir = './uploads/';
+$path_parts = pathinfo($image['name']);
 
 $current_link = $image['tmp_name'];
-$new_link     = $upload_dir.uniqid().'_'.$image['name'];
+$new_link     = $upload_dir.$path_parts['filename'].'_'.uniqid().'.'.$path_parts['extension'];
 
 move_uploaded_file($current_link, $new_link);
 
 
-$image_link_exist_in_db = is_link_exist_in_db($new_link);
-
-if ( ! $image_link_exist_in_db) {
-    add_image_link_to_db($new_link);
-}
+add_image_link_to_db($new_link);
 
 
 unset($_FILES['img_input']);
+
 
 redirect_to('./task_15.php');
 
