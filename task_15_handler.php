b7f4b49ae80079852c13c1176f793a689e4759c6
xrr <xrr@email.com>
2021-11-20 16:47:48 +0300

    15 commit

--- task_15_handler.php
+++ task_15_handler.php
@@ -57,7 +57,7 @@ if (isset($_FILES['img_input'])) {
 $upload_dir = './uploads/';
 
 $current_link = $image['tmp_name'];
-$new_link     = $upload_dir.$image['name'];
+$new_link     = $upload_dir.uniqid().'_'.$image['name'];
 
 move_uploaded_file($current_link, $new_link);
 
